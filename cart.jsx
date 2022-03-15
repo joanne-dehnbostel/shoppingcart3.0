82f0d418c9738bca40ab84cddbbcc74f65e7f6ba
Joanne Dehnbostel <joanne.dehnbostel@gmail.com>
2022-03-15 00:57:16 -0500

    Update cart.jsx

--- cart.jsx
+++ cart.jsx
@@ -249,15 +249,4 @@ const Products = (props) => {
 };
 // ========================================
 ReactDOM.render(<Products />, document.getElementById("root"));
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
